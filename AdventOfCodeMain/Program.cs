5dd3f7940b95984e1fe1fb97b9686dd004cc4d34
plohich <be.pop@abv.bg>
2025-03-05 00:23:59 +0200

    Implementing Quick Sort As Exercise

--- AdventOfCodeMain/Program.cs
+++ AdventOfCodeMain/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using QuickSortExercise;
+
 namespace AdventOfCode;
 using Microsoft.Extensions.Configuration;
 
@@ -22,6 +24,11 @@ class Program
 
         // AdventOfCode_202414.Executor.Execute_1(pathToInput);
         
-        AdventOfCode_202415.Executor.Execute_1(pathToInput);
+        //AdventOfCode_202415.Executor.Execute_1(pathToInput);
+        int[] myArr = [4, 5, 23, 45, 3, 56, 12, 6, 3, 57, 90, 22, 11, 7, 38, 13, 21, 18, 43];
+        QuickSortExercise.SortWorker.ApplyQuickSort(myArr);
+        SortWorker.Print(myArr);
+      
+    
     }
 }
---
+++ QuickSortExercise/SortWorker.cs
@@ -0,0 +1,47 @@
+﻿namespace QuickSortExercise;
+
+public static class SortWorker
+{
+    public static int[] ApplyQuickSort(int[] inputArray) => QuickSort(inputArray, 0, inputArray.Length - 1);
+    
+    private static int[] QuickSort(int[] inputArray, int idxStart, int idxEnd)
+    {
+        if (idxStart < idxEnd)
+        {
+            int pi = Partition(inputArray, idxStart, idxEnd);
+            QuickSort(inputArray, idxStart, pi - 1);
+            QuickSort(inputArray, pi + 1, idxEnd);
+        }
+
+        return inputArray;
+        
+    }
+
+    private static int Partition(int[] inputArray, int idxStart, int idxEnd)
+    {
+        int pivotIdx = idxEnd;
+        int lowerElementIdx = idxStart - 1;
+        for (int i = idxStart; i < idxEnd; i++)
+        {
+            if (inputArray[i] <= inputArray[pivotIdx])
+            {
+                lowerElementIdx++;
+                SwapElements(ref inputArray[lowerElementIdx], ref inputArray[i]);
+            }
+        }
+        
+        SwapElements(ref inputArray[lowerElementIdx + 1], ref inputArray[pivotIdx]);
+        return lowerElementIdx + 1;
+    }
+
+    public static void Print(int[] inputArray)
+    {
+        foreach (var entry in inputArray)
+        {
+            Console.Write($"{entry} ");
+        }
+        Console.WriteLine();
+    }
+
+    private static void SwapElements(ref int a, ref int b) => (a, b) = (b, a);   
+}
